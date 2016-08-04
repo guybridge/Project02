9ac6acba743f00b38240bf880d3040adcc68bf1f
Guy Bridge <guy@wsit.com.au>
2016-08-04 09:26:06 +0800

    init

---
+++ src/LeagueManager.java
@@ -0,0 +1,17 @@
+/**
+ * Created by guyb on 4/08/16.
+ */
+import com.teamtreehouse.model.Player;
+import com.teamtreehouse.model.Players;
+
+public class LeagueManager {
+
+    public static void main(String[] args) {
+        Player[] players = Players.load();
+        System.out.printf("There are currently %d registered players.%n", players.length);
+        // Your code here!
+
+
+    }
+
+}
